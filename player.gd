b66a905829b838d24e5a4f721dbb394b1493a39b
angie <88114622+angiefn@users.noreply.github.com>
2023-12-19 11:18:32 -0800

    collision setup

--- player.gd
+++ player.gd
@@ -2,10 +2,6 @@ extends Area2D
 
 @export var Speed = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,11 +13,14 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * Speed
 		
-	velocity += velocity * delta
-		
+	position += velocity * delta
+	
+
+func _on_body_entered(body):
+	hide() # Replace with function body.
